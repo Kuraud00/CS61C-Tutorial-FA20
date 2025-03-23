56f31340cb5f2b1527ad54ca71ab59637ee26968
Kuraud00 <2756345706@qq.com>
2025-03-23 12:13:53 +0800

    fin:lab01

--- labs/lab01/hello.c
+++ labs/lab01/hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+void welcome(){
+    printf("Welcome to gdb!");
+    return ;
+}
+
 int main(int argc, char *argv[]) {
     int i;
     int count = 0;
@@ -8,6 +13,8 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < 10; i++) {
         (*p)++; // Do you understand this line of code and all the other permutations of the operators? ;)
     }
+    
+    welcome();
 
     printf("Thanks for waddling through this program. Have a nice day.");
     return 0;
--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if(head == NULL){return 0;}
+    node *tortoise = head;
+    node *hare = head;
+    while(1){
+	hare = hare->next;
+        if(hare == NULL){return 0;}
+	hare = hare->next;
+	if(hare == NULL){return 0;}
+	tortoise = tortoise->next;
+	if(tortoise == hare){return 1;}
+    }
 }
