d74de56be86cfcf7dc40aa2fbfdc6862fcf377a9
Kuraud00 <2756345706@qq.com>
2025-03-24 18:51:05 +0800

    fin:lab02

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -4,7 +4,14 @@
 #include <string.h>
 #include "lfsr.h"
 
+uint16_t get_bit(uint16_t reg,uint16_t n){
+    return (reg>>n) & 1;
+}
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t i = (get_bit(*reg,0)^get_bit(*reg,2)^get_bit(*reg,3)^get_bit(*reg,5))<<15;
+    *reg = (*reg>>1);
+    *reg |= i;
 }
 
